85fc88a8dc8b57461e05d78e4abf154c097f3c67
StanislavAlpatiev <alpatiev.stanislav@gmail.com>
2021-11-29 11:25:53 +0100

    changed names of files to uppercase

--- src/label.cpp
+++ src/label.cpp
@@ -5,22 +5,25 @@
 #include <string>
 using namespace std;
 
-namespace cwing {
+namespace cwing
+{
 
-	Label* Label::getInstance(int x, int y, int w,
-		int h, std::string txt) {
+	Label *Label::getInstance(int x, int y, int w,
+							  int h, std::string txt)
+	{
 		return new Label(x, y, w, h, txt);
 	}
 
 	Label::Label(int x, int y, int w, int h,
-		std::string txt): Component(x,y,w,h), text(txt)
+				 std::string txt) : Component(x, y, w, h), text(txt)
 	{
-		SDL_Surface* surf = TTF_RenderText_Solid(sys.get_font(), text.c_str(), { 0,0,0 });
+		SDL_Surface *surf = TTF_RenderText_Solid(sys.get_font(), text.c_str(), {0, 0, 0});
 		texture = SDL_CreateTextureFromSurface(sys.get_ren(), surf);
 		SDL_FreeSurface(surf);
 	}
 
-	void Label::draw() const {
+	void Label::draw() const
+	{
 		SDL_RenderCopy(sys.get_ren(), texture, NULL, &getRect());
 	}
 
@@ -29,14 +32,16 @@ namespace cwing {
 		SDL_DestroyTexture(texture);
 	}
 
-	string Label::getText() const {
+	string Label::getText() const
+	{
 		return text;
 	}
-	void Label::setText(std::string newText) {
+	void Label::setText(std::string newText)
+	{
 		text = newText;
 		SDL_DestroyTexture(texture);
-		SDL_Surface* surf = TTF_RenderText_Solid(sys.get_font(),
-			text.c_str(), { 0,0,0 });
+		SDL_Surface *surf = TTF_RenderText_Solid(sys.get_font(),
+												 text.c_str(), {0, 0, 0});
 		texture = SDL_CreateTextureFromSurface(sys.get_ren(), surf);
 		SDL_FreeSurface(surf);
 	}
