5d294c34fd1a9b920ca1518314b94207bc336099
verafied <verasolnygren@gmail.com>
2021-12-26 18:21:05 +0100

    made objects not depend on events to move

--- src/Session.cpp
+++ src/Session.cpp
@@ -14,27 +14,28 @@ namespace cwing
 	void Session::run()
 	{
 		bool quit = false;
-		const int tickInterval = 1000/FPS;
+		const int tickInterval = 1000 / FPS;
 		while (!quit)
 		{
 			Uint32 nextTick = SDL_GetTicks() + tickInterval;
 			SDL_Event event;
 			while (SDL_PollEvent(&event))
 			{
-				
-				switch(event.type){
-					case SDL_QUIT:
-						quit = true;
-						break;
-				}
 
-
-				for (Component *c : comps)
+				switch (event.type)
 				{
-					c->perform(event);
-					if(c->isKilled()){
-						//TODO: remove from comps e.t.c., and then use its deconstructor.
-					}
+				case SDL_QUIT:
+					quit = true;
+					break;
+				}
+			}
+
+			for (Component *c : comps)
+			{
+				c->perform(event);
+				if (c->isKilled())
+				{
+					//TODO: remove from comps e.t.c., and then use its deconstructor.
 				}
 			}
 			// 	switch (event.type)
@@ -65,17 +66,18 @@ namespace cwing
 			//
 			SDL_SetRenderDrawColor(sys.get_ren(), 255, 255, 255, 255);
 			SDL_RenderClear(sys.get_ren());
-			for (Component *c : comps){
+			for (Component *c : comps)
+			{
 				c->draw();
 			}
 			SDL_RenderPresent(sys.get_ren());
 			int delay = nextTick - SDL_GetTicks();
-			if(delay >0){
+			if (delay > 0)
+			{
 				SDL_Delay(delay);
 			}
 
 		} //yttre while
-
 	}
 
 	Session::~Session()
