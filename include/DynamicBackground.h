d9295c646e6777b30e27f28ce423f33bc59106d7
verafied <verasolnygren@gmail.com>
2022-01-03 15:18:16 +0100

    refactoring

--- include/DynamicBackground.h
+++ include/DynamicBackground.h
@@ -6,14 +6,13 @@
 
 namespace cwing
 {
-
     class DynamicBackground : public Sprite
     {
     public:
         static DynamicBackground *getInstance(int x, int y, int w, int h, std::string image_path);
-        Component* perform(SDL_Event event);
+        Component *perform(SDL_Event event);
         void moveLeft();
-        void removal() {this->~DynamicBackground();};
+        void removal() { this->~DynamicBackground(); };
         void draw() const;
         void setAngle(const int &angle);
         void spinImage(int &SPEED);
@@ -23,7 +22,6 @@ namespace cwing
         DynamicBackground(int x, int y, int w, int h, std::string images_path);
 
     private:
-        SDL_Texture *sprite;
         int speedCounter = 0;
         int moveEveryXFrame, spinDir;
         float angle = 0;
